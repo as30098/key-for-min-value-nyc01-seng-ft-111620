17f307482e97e3141d2525e5d091b4472314419b
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-12 14:09:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   key_for_min_value = nil
   name_hash.each do |key, value| 
-    if min_value < value || min_value == nil
+    if value < min_value || min_value == nil
       min_value = value
       key_for_min_value = key
     end 
